1867318712b896511be6e54395fd82335c00b0e9
Parth <parolekarparth@gmail.com>
2023-03-02 15:41:56 +0530

    authentication with google and email/password

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,37 @@
 import React from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
 import { Link } from 'react-router-dom'
+import { auth } from '../config/firebase'
+import { signOut } from 'firebase/auth'
 
 const Navbar = () => {
+  const [user] = useAuthState(auth)
+
+  const signUserOut = async () => {
+    await signOut(auth)
+  }
+  //   console.log(user)
   return (
-    <div>
-      <Link to='/'>
-        <h3>Home</h3>
-      </Link>
-      <Link to='/login'>
-        <h3>Login</h3>
-      </Link>
+    <div className='navbar'>
+      <div className='links'>
+        <Link to='/'>Home</Link>
+        <Link to='/login'>Login</Link>
+      </div>
+      <div className='user'>
+        {user && (
+          <>
+            <p> {user?.displayName} </p>
+            <p> {user?.email} </p>
+            <img
+              src={user?.photoURL || ''}
+              width='20'
+              height='20'
+              alt='display'
+            />
+            <button onClick={signUserOut}> Log Out</button>
+          </>
+        )}
+      </div>
     </div>
   )
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,116 @@
-import React from 'react'
+import {
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from 'firebase/auth'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { auth, googleProvider } from '../config/firebase'
 
 const Login = () => {
-  return <div>Login</div>
+  const navigate = useNavigate()
+
+  const [signUp, setSignUp] = useState({ email: '', password: '' })
+  const [signIn, setSignIn] = useState({ email2: '', password2: '' })
+
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider)
+      console.log(result)
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const signUpCredentials = (e: React.ChangeEvent) => {
+    setSignUp({
+      ...signUp,
+      [(e.target as HTMLInputElement).id]: (e.target as HTMLInputElement).value
+    })
+  }
+  const signInCredentials = (e: React.ChangeEvent) => {
+    setSignIn({
+      ...signIn,
+      [(e.target as HTMLInputElement).id]: (e.target as HTMLInputElement).value
+    })
+  }
+
+  const signUpWithEmail = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        signUp.email,
+        signUp.password
+      )
+      console.log(user)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const signInWithEmail = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        signIn.email2,
+        signIn.password2
+      )
+      console.log(user.user)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <button onClick={signInWithGoogle}>Sign in with google</button>
+      <div>
+        <h3>Sign Up</h3>
+        <div>
+          <label htmlFor='email'>Email </label>
+          <input
+            type='text'
+            id='email'
+            value={signUp.email}
+            onChange={signUpCredentials}
+          />
+        </div>
+        <div>
+          <label htmlFor='password'>Password </label>
+          <input
+            type='password'
+            id='password'
+            value={signUp.password}
+            onChange={signUpCredentials}
+          />
+        </div>
+        <button onClick={signUpWithEmail}>Sign Up</button>
+      </div>
+      <div>
+        <h3>Sign In</h3>
+        <div>
+          <label htmlFor='email'>Email </label>
+          <input
+            type='text'
+            id='email2'
+            value={signIn.email2}
+            onChange={signInCredentials}
+          />
+        </div>
+        <div>
+          <label htmlFor='password'>Password </label>
+          <input
+            type='password'
+            id='password2'
+            value={signIn.password2}
+            onChange={signInCredentials}
+          />
+        </div>
+        <button onClick={signInWithEmail}>Sign In</button>
+      </div>
+    </div>
+  )
 }
 
 export default Login
